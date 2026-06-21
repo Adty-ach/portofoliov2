1d7b327c95794eb6d7a15cdcefefaa917d571058
Adty-ach <Adityagt49@gmail.com>
2026-06-21 22:23:31 +0700

    Add Google Analytics

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import './styles/globals.css';
+import ReactGA from "react-ga4";
+
+ReactGA.initialize("G-BX926C5S94");
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
